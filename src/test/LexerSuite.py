527b2470613598e3c51630df5f4d44befdf7334b
WindyFeg <phongspeed7@yahoo.com.vn>
2023-01-16 11:36:36 +0700

    First init and do specification

---
+++ src/test/LexerSuite.py
@@ -0,0 +1,9 @@
+import unittest
+from TestUtils import TestLexer
+
+
+class LexerSuite(unittest.TestCase):
+
+    def test_lowercase_identifier(self):
+        """test identifiers"""
+        self.assertTrue(TestLexer.test("abc", "abc,<EOF>", 101))
---
+++ src/test/ParserSuite.py
@@ -0,0 +1,10 @@
+import unittest
+from TestUtils import TestParser
+
+
+class ParserSuite(unittest.TestCase):
+    def test_simple_program(self):
+        """Simple program: int main() {} """
+        input = """main: function void() {}"""
+        expect = "successful"
+        self.assertTrue(TestParser.test(input, expect, 201))
